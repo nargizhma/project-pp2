2d600d9516a6f75097449f2abbf51390691d8258
Sevil Babayeva <sevilsb@code.edu.az>
2026-05-03 20:26:48 +0400

    added new variety of products

--- src/main/java/com/siliconSpatula/model/MenuItemFactory.java
+++ src/main/java/com/siliconSpatula/model/MenuItemFactory.java
@@ -15,13 +15,16 @@ public class MenuItemFactory {
             case "FRIES":          return new HotFood.Fries();
             case "MUFFIN_CAKE":    return new Dessert.MuffinCake();
             case "CHEESECAKE":     return new Dessert.Cheesecake();
-            case "COFFEE":         return new Beverage.HotDrink.Coffee();
+            case "LATTE":          return new Beverage.HotDrink.Latte();
+            case "AMERICANO":      return new Beverage.HotDrink.Americano();
             case "TEA":            return new Beverage.HotDrink.Tea();
             case "COLA":           return new Beverage.ColdDrink.Cola();
             case "WATER":          return new Beverage.ColdDrink.Water();
             case "KETCHUP_SAUCE":  return new Sauce.KetchupSauce();
             case "MAYO_SAUCE":     return new Sauce.MayoSauce();
             case "BARBECUE_SAUCE": return new Sauce.BarbecueSauce();
+            case "SPRITE":          return new Beverage.ColdDrink.Sprite();
+            case "COLAZERO":        return new Beverage.ColdDrink.ColaZero();
             default:
                 throw new IllegalArgumentException("Unknown menu item token: " + token);
         }
@@ -36,13 +39,16 @@ public class MenuItemFactory {
             new HotFood.Fries(),
             new Dessert.MuffinCake(),
             new Dessert.Cheesecake(),
-            new Beverage.HotDrink.Coffee(),
+            new Beverage.HotDrink.Latte(),
+            new Beverage.HotDrink.Americano(),
             new Beverage.HotDrink.Tea(),
             new Beverage.ColdDrink.Cola(),
             new Beverage.ColdDrink.Water(),
             new Sauce.KetchupSauce(),
             new Sauce.MayoSauce(),
-            new Sauce.BarbecueSauce()
+            new Sauce.BarbecueSauce(),
+            new Beverage.ColdDrink.Sprite(),
+            new Beverage.ColdDrink.ColaZero()
         };
     }
 }
